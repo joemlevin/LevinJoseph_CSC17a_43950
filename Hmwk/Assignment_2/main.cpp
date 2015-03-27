2939269d6bffe09e4ff63a885349d10f8ee72e53
joemlevin <joemlevin@gmail.com>
2015-03-26 18:47:53 -0700

    cleaned up project a bit

--- Hmwk/Assignment_2/main.cpp
+++ Hmwk/Assignment_2/main.cpp
@@ -37,38 +37,6 @@ int main(int argc, char** argv) {
     for(int i=0;i<(m[0]+2);i++)
         cout<<m[i]<<" ";
     cout<<endl;
-    
-    
-//    bubSort(a,n);
-//    for(int i=0;i<n;i++)
-//        cout<<a[i]<<" ";
-//    cout<<endl;
-//    int val=a[0];//for tracking which values are modes
-//    int count=0, freq=0, numMode=1;
-//    for(int i=0;i<=n;i++){
-//        if(a[i]==val)
-//            count++;
-//        else{
-//            if(count>freq){
-//                freq=count;
-//                count=1;
-//                numMode=1;
-//                val=a[i];
-//            }
-//            else if(count==freq){
-//                numMode++;
-//                val=a[i];
-//                count=1;
-//            }
-//            else{
-//                val=a[i];
-//                count=1;
-//            }
-//        }
-//    }
-//    cout<< "# of modes: " <<numMode<<endl;
-//    cout<<"Frequency: "<<freq<<endl;
-
     return 0;
 }
 
@@ -120,59 +88,9 @@ int *mode(int a[],int n){
             val=a[i];
         }
 }
+    //end
     return m;
 }
-//int *mode(int a[], int n){
-//    //copy given array into dynamic array and sort
-//    int *b=new int[n];
-//    int freq=-1; //tracks the highest frequency found
-//    int val=-1;//determines the value being counted
-//    int count=0; //counts occurrences of each value in array
-//    int posMode; //possible mode in array
-//    int numMode=0; //number of modes in array
-//    for(int i=0;i<n;i++){
-//        b[i]=a[i];
-//    }
-//    bubSort(b,n);
-//    //loop through sorted array and determine modes
-//    for(int i=0;i<n;i++){
-//        if(b[i]!=val){
-//            if(count>freq){ //value counted previously occurs more often
-//                freq=count; //replaces highest frequency with new count
-//                posMode=b[i]; //replaces possible mode with new value
-//                numMode=1; //resets the number of modes found
-//            }
-//            else if(count==freq){
-//                numMode++; //increments the number of modes found
-//            }
-//            else //count<freq
-//                posMode=posMode; //the possible stays the same
-//            val=b[i];
-//            count=0; //resets count to zero for next value
-//        }
-//        else
-//            count++;
-//    }
-//    //create dynamic array for storing information regarding mode
-//        int *m=new int[numMode+2]; //stores size, frequency, and values of modes
-//        m[0]=numMode; //how many modes there are
-//        m[1]=freq; //frequency of occurrence
-//        count=0; //reuse count for determining which values are modes
-//        val=-1;
-//        int index=2;//stores values with freq of mode into location in m
-//        for(int i=0;i<n;i++){
-//            if(b[i]!=val){
-//                val=b[i];
-//                count=0;
-//            }
-//            else
-//                count++;
-//            if(count==freq)
-//                m[index]=val;
-//        }
-//        return m;
-//    
-//}
 void bubSort(int *a,int size){
     for(int i=0; i<size; i++)
     {
