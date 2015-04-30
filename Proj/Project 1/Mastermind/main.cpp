45352e0af0b4c57dd90967e0327b16bda0bd2886
joemlevin <joemlevin@gmail.com>
2015-04-29 17:17:47 -0700

    getG almost finished

--- Proj/Project 1/Mastermind/main.cpp
+++ Proj/Project 1/Mastermind/main.cpp
@@ -10,6 +10,8 @@
 #include <iostream>
 #include <iomanip>
 #include <ctime>
+#include <string>
+#include <cstring>
 
 using namespace std;
 
@@ -21,14 +23,16 @@ using namespace std;
 //Function Prototypes
 Answer *getAns(int,int);//generates an answer
 void checkG(Answer *,Guess *,int);//checks guess against answer
+int *getG(int);//gets guess from user
+
+//Begin
 int main(int argc, char** argv) {
     
-    srand(time(0));
-        Answer *test=getAns(10,4);
-    Guess *temp=new Guess;
-    temp->code=new int[4];
-    cin>>temp->code;
-
+//    srand(time(0));
+//        Answer *test=getAns(10,4);
+//    Guess *temp=new Guess;
+//    temp->code=new int[4];
+    
     return 0;
 }
 
@@ -82,4 +86,43 @@ void checkG(Answer *answer,Guess *guess,int row){
     }
     delete []ta;
     delete []tg;
+}//end
+/*
+ * getG prompts the user to enter a 4 digit combination, separated by spaces
+ * it stores it as a string, and checks if every odd element is a digit 1-8
+ * and then checks if every even is a space
+ * if this is true, it then converts each odd element into an int and stores
+ * it in an int array and returns this array
+ * takes an int to specify how big an int array to create
+ */
+int *getG(int row){
+    //declare variables
+    int *temp=new int[row];//will be returned
+    string guess;//for character checking
+    bool check;//input validation
+    cout<<"Enter a "<<row
+            <<" digit combination (1-8) separated by spaces."<<endl;
+    do{
+        check=true;
+        cin>>guess;
+        if(guess.size()!=2*row-1){//row digits plus spaces between
+            cout<<"You must enter "<<row<<" digits separated by spaces."
+                    <<"Try again."<<endl;
+            bool check=false;
+        }
+        else{
+            for(int i=0;i<2*row-1;i++){
+                if(i%2!=0&&!isdigit(guess[i]))//every odd must be digit
+                    check=false;
+                else if(i%2!=0&&(atoi(&guess[i])>8||atoi(&guess[i])<1))
+                    check=false;
+                else{
+                    if(!isspace(guess[i]))
+                        check=false;
+                }
+            }   
+        }
+        for(int i=0;i<row;i++)
+            temp[i]=atoi(&guess[i+1])
+    }while(!check);
 }
