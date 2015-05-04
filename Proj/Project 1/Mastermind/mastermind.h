27f910ee1dbc32897a4f8da59204d58e62998e20
Joseph Levin <jlevin@student.rcc.edu>
2015-05-04 00:59:42 -0700

    Project 1
    Moooore doxygen

--- Proj/Project 1/Mastermind/mastermind.h
+++ Proj/Project 1/Mastermind/mastermind.h
@@ -8,27 +8,41 @@
 #ifndef MASTERMIND_H
 #define	MASTERMIND_H
 
+/*!
+ * Answer stores the correct code in an int array and the max guesses allowed
+ */
 struct Answer{
-    int *code;//stores the integer combination
-    int mxGuess;//keeps track of maximum guesses allowed
+    int *code;//!stores the integer combination
+    int mxGuess;//!keeps track of maximum guesses allowed
     
 };
 
+/*!
+ * Guess stores a guess in an int array, the number of correct positions
+ * associated with the guess and the number of correct numbers
+ */
 struct Guess{
-    int *code;//stores the guess
-    int corPos;//tallies the number of correct positions in a guess
-    int corNum;//tallies the number of correct numbers in a guess
+    int *code;//!stores the guess
+    int corPos;//!tallies the number of correct positions in a guess
+    int corNum;//!tallies the number of correct numbers in a guess
 };
 
+/*!
+ * Guesses stores a pointer a Guess (one for each turn) and
+ * the total number of guesses taken
+ */
 struct Guesses{
-    Guess *guess;//array of guess attempts
-    int nGuess;//how many guesses have been taken
+    Guess *guess;//!array of guess attempts
+    int nGuess;//!how many guesses have been taken
 };
 
+/*!
+ * Stats stores the number of wins, losses, and number of guesses
+ */
 struct Stats{
-    int wins;//number of wins
-    int loses;//number of loses
-    int nGuess;//number of loses
+    int wins;//!number of wins
+    int loses;//!number of loses
+    int nGuess;//!number of guesses
 };
 
 
